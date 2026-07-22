0c07e6f7fd6956295ad930c676745d7648b47437
Cookie774-GameDev <vipersel2@gmail.com>
2026-07-22 14:43:37 -0400

    Add operator publishing API route

--- app/api/operator-overview/route.ts
+++ app/api/operator-overview/route.ts
@@ -1,2 +1,4 @@
 import handler from '../../../netlify/functions/operator-overview.mjs';
+
 export const GET = (request: Request) => handler(request);
+export const POST = (request: Request) => handler(request);
